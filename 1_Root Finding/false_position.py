af7337e41df014159427895d227160827cd9f8e2
Loyalhand <47873075+Loyalhand@users.noreply.github.com>
2021-01-20 23:00:47 -0600

    added false positon method
    python

--- 1_Root Finding/false_position.py
+++ 1_Root Finding/false_position.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 a = 0 
 b = 1
 tol = .0000001
@@ -5,12 +10,27 @@ tol = .0000001
 def f(x):
     return x**3 + 2*x - 2 
 
-while (b-a)/2 >= tol:
-    c = b-f(b)*(b-a)/(f(b)-f(a))
-    if f(c)>0:
-        b = c
-    if f(c)<0:
-        a = c 
+c1 = b-f(b)*(b-a)/(f(b)-f(a))
+cl = [c1]
+if f(c1) > 0:
+        b = c1
+if f(c1) < 0:
+        a = c1 
 
-x = (a+b)/2
-print(x)
+c2 = b-f(b)*(b-a)/(f(b)-f(a))
+if f(c2) > 0:
+        b = c2
+if f(c2) < 0:
+        a = c2 
+cl.append(c2)
+
+i = 1
+while abs(cl[i] - cl[i-1]) >= tol:
+    c = b-f(b)*(b-a)/(f(b)-f(a))
+    if f(c) > 0:
+        b = c
+    if f(c) < 0:
+        a = c 
+    cl.append(c)
+    i += 1
+print(c)
