70df46cf05085ef246fe5801765a7289e977981e
Pahayo, Gari M. (S&T-Student) <gmpckv@mst.edu>
2021-05-05 02:01:52 -0500

    hw7 update

--- Assignments/hw7/driver.m
+++ Assignments/hw7/driver.m
@@ -6,34 +6,36 @@ format longg
 %=========================================================================
                                 %q1
 %=========================================================================                                
-xstart = 1;
-xend = 2;
-xstart = 0;
-xend = pi;
-Trap = zeros(20,1);
-Simp = Trap;
-meshSize = Simp;
-I = pi*ones(20,1);
-i = 1;
-for intervals = 2:2:40
-    [range,xpoints] = getRange(xstart,xend,intervals);
-    h = xpoints(2)-xpoints(1);
-    meshSize(i,1) = h;
-    Trap(i,1) = Trapz(h,intervals,range);
-    Simp(i,1) = Simp13(h,intervals,range);
-    i = i + 1;
-end
+% xstart = 1;
+% xend = 2;
+% xstart = 0;
+% xend = pi;
+% Trap = zeros(20,1);
+% Simp = Trap;
+% meshSize = Simp;
+% I = pi*ones(20,1);
+% i = 1;
+% for intervals = 2:2:40
+%     [range,xpoints] = getRange(xstart,xend,intervals);
+%     h = xpoints(2)-xpoints(1);
+%     meshSize(i,1) = h;
+%     Trap(i,1) = Trapz(h,intervals,range);
+%     Simp(i,1) = Simp13(h,intervals,range);
+%     i = i + 1;
+% end
+% 
+% errTrap = abs(I-Trap);
+% errSimp = abs(I-Simp);
+% 
+% hold on 
+% set(gca, 'XScale', 'log', 'YScale', 'log');
+% loglog(meshSize,errTrap)
+% loglog(meshSize,errSimp)
+% legend('Trapezoidal','Simpson 1/3','location','northwest')
+% title('Error vs.Mesh size')
+% ylabel('error')
+% xlabel('mesh size')
+% grid on
+% hold off
 
-errTrap = abs(I-Trap);
-errSimp = abs(I-Simp);
-
-hold on 
-set(gca, 'XScale', 'log', 'YScale', 'log');
-loglog(meshSize,errTrap)
-loglog(meshSize,errSimp)
-legend('Trapezoidal','Simpson 1/3','location','northwest')
-title('Error vs.Mesh size')
-ylabel('error')
-xlabel('mesh size')
-grid on
-hold off
+gaussQuad(0,pi,4)
---
+++ Assignments/hw7/gaussQuad.m
@@ -0,0 +1,29 @@
+function I = gaussQuad(a,b,n)
+%myFun - Description
+%
+% Syntax: output = myFun(input)
+%
+% Long description
+
+switch n
+    case 2
+        c = ones(2,1)
+        t = [-.57735;.57735]
+    case 3 
+        c = [.555556;.888889;.555556]
+        t = [-.774597;0;.774597]
+    case 4   
+        c = [.347855;.652145;.652145;.347855]  
+        t = [-.861136;-.339981;.339981;.861136]
+    otherwise 
+        disp('nope')
+end
+
+x = zeros(n,1)
+for i = 1:n
+    x(i,1) = (b+a)/2-(b-a)/2*t(i)
+end
+I = 0
+for i = 1:n
+    I = (b-a)/2*c(i)*f(x(i)) + I
+end
